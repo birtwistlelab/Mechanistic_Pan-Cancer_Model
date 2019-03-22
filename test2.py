870321f04efa1f093105aca46d0aab258d5c0adf
Ashwin Srinath <shwina@users.noreply.github.com>
2019-03-22 09:21:22 -0400

    Make simpler test case

--- test2.py
+++ test2.py
@@ -10,7 +10,7 @@ np.set_printoptions(threshold=sys.maxsize)
 
 
 # deterministic
-th=12;
+th=0.1;
 flagD=1;
 [dataS, dataG] = RunPrep()
 STIM = np.zeros(shape = (775));
@@ -19,26 +19,5 @@ STIM [84-1] = 0.00385
 np.savetxt('t_deterministic.csv', t, delimiter=',')
 np.savetxt('xoutG_deterministic.csv', xoutG, delimiter=',')
 np.savetxt('xoutS_deterministic.csv', xoutS, delimiter=',')
-
-
-
-
-# stochastic
-th=12;
-flagD=0
-STIM = np.zeros(shape = (775));
-STIM[156-1:162]=[3.3,100,100,100,100,100,1721]
-[dataS, dataG] = RunPrep()
-[t, xoutG, xoutS] = RunModel(flagD, th, STIM, [], [], dataS, dataG, dataG.kTCleak, dataG.kTCmaxs)
-np.savetxt('t_stochastic.csv', t, delimiter=',')
-np.savetxt('xoutG_stochastic.csv', xoutG, delimiter=',')
-np.savetxt('xoutS_stochastic.csv', xoutS, delimiter=',')
-
-
-
-
-
-
-
 
 print("done ")
