a370c29a0ba2fe1183c9d09dda25890b8f8a6cf2
Ashwin Srinath <shwina@users.noreply.github.com>
2019-03-22 15:10:08 -0400

    Add reference test results and comparison script

---
+++ diff.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+G = np.loadtxt('xoutG_deterministic.csv', delimiter=',')
+S = np.loadtxt('xoutS_deterministic.csv', delimiter=',')
+
+ref_G = np.loadtxt('reference/xoutG_deterministic.csv', delimiter=',')
+ref_S = np.loadtxt('reference/xoutS_deterministic.csv', delimiter=',')
+
+np.testing.assert_allclose(G, ref_G)
+np.testing.assert_allclose(S, ref_S)
--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,2 @@
 python test2.py
-
-echo "Got:"
-md5 t_deterministic.csv xoutG_deterministic.csv xoutS_deterministic.csv
-echo "----"
-echo "Expect:"
-cat md5sums.txt
-
-rm -f t_deterministic.csv xoutG_deterministic.csv xoutS_deterministic.csv
+python diff.py
