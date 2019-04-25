8638139b23d6651dfcc8fb049ca75fd0ca41c326
Ethan Bensman <ebenz99@gmail.com>
2019-04-24 20:40:52 -0400

    reverted

--- test2.py
+++ test2.py
@@ -2,17 +2,13 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-import os
-from random import randint
 from RunPrep import *
 from RunModel import *
 
 
 np.set_printoptions(threshold=np.nan)
-cdir = os.getcwd()
-ext = str(randint(1,10000))		#will likely be fixed with command line args in future versions
-ndir = cdir + "/op/"+ext
-os.makedirs(ndir,exist_ok=True)
+
+
 
 # deterministic
 th=12;
@@ -21,9 +17,9 @@ flagD=1;
 STIM = np.zeros(shape = (775));
 STIM [84-1] = 0.00385
 [t, xoutG, xoutS] = RunModel(flagD, th, STIM, [], [], dataS, dataG, dataG.kTCleak, dataG.kTCmaxs)
-np.savetxt(ndir+'/t_deterministic.csv', t, delimiter=',')
-np.savetxt(ndir+'/xoutG_deterministic.csv', xoutG, delimiter=',')
-np.savetxt(ndir+'/xoutS_deterministic.csv', xoutS, delimiter=',')
+np.savetxt('t_deterministic.csv', t, delimiter=',')
+np.savetxt('xoutG_deterministic.csv', xoutG, delimiter=',')
+np.savetxt('xoutS_deterministic.csv', xoutS, delimiter=',')
 
 
 
@@ -35,9 +31,13 @@ STIM = np.zeros(shape = (775));
 STIM[156-1:162]=[3.3,100,100,100,100,100,1721]
 [dataS, dataG] = RunPrep()
 [t, xoutG, xoutS] = RunModel(flagD, th, STIM, [], [], dataS, dataG, dataG.kTCleak, dataG.kTCmaxs)
-np.savetxt(ndir+'/t_stochastic.csv', t, delimiter=',')
-np.savetxt(ndir+'/xoutG_stochastic.csv', xoutG, delimiter=',')
-np.savetxt(ndir+'/xoutS_stochastic.csv', xoutS, delimiter=',')
+np.savetxt('t_stochastic.csv', t, delimiter=',')
+np.savetxt('xoutG_stochastic.csv', xoutG, delimiter=',')
+np.savetxt('xoutS_stochastic.csv', xoutS, delimiter=',')
+
+
+
+
 
 
 
